1ce610a2ee8f5b81497c0e05ddce78b713a23c78
riyaverma338 <vermariya1202@gmail.com>
2021-10-03 00:49:46 +0530

    update

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 
 // var navbar = document.querySelector('nav')
@@ -31,18 +31,35 @@ function showSlides() {
 // }
 
 const myFunction = () => {
-  document.getElementById("first").style.display ='block';
-  document.getElementById("second").style.display ='none'
-  document.getElementById("third").style.display ='none'
+  document.querySelector("first").style.display ='block';
+  document.querySelector("second").style.display ='none';
+  document.querySelector("third").style.display ='none';
+  document.querySelector("fourth").style.display ='none'
 }
 
 const myFunction2 = () => {
-  document.getElementById("second").style.display ='block'
-  document.getElementById("first").style.display ='none'
-  document.getElementById("third").style.display ='none'
+  document.querySelector("second").style.display ='none';
+  document.querySelector("first").style.display ='none';
+  document.querySelector("third").style.display ='none';
 }
 
 const myFunction3 = () => {
+  document.getElementById("third").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+}
+
+const myFunction4 = () => {
+  document.getElementById("third").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+}
+const myFunction5 = () => {
+  document.getElementById("third").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+}
+const myFunction6 = () => {
   document.getElementById("third").style.display ='block'
   document.getElementById("first").style.display ='none'
   document.getElementById("second").style.display ='none'
